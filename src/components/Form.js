0d43a741aed314391e58da01f81dc73310b6a299
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2023-07-30 12:23:00 +0200

    2 handling events

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,39 @@
 function Form() {
+	const resetHandler = () => {
+		console.log("RESET");
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		console.log("SUBMIT");
+	};
+
+	const inputHandler = (input, value) => {
+		console.log(input, value);
+	};
+
 	return (
-		<form className="form">
+		<form className="form" onSubmit={submitHandler}>
 			<div className="input-group">
 				<p>
 					<label htmlFor="current-savings">Current Savings ($)</label>
-					<input type="number" id="current-savings" />
+					<input
+						onChange={(e) => {
+							inputHandler("current-savings", e.target.value);
+						}}
+						type="number"
+						id="current-savings"
+					/>
 				</p>
 				<p>
 					<label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-					<input type="number" id="yearly-contribution" />
+					<input
+						onChange={(e) => {
+							inputHandler("yearly-contribution", e.target.value);
+						}}
+						type="number"
+						id="yearly-contribution"
+					/>
 				</p>
 			</div>
 			<div className="input-group">
@@ -16,15 +41,27 @@ function Form() {
 					<label htmlFor="expected-return">
 						Expected Interest (%, per year)
 					</label>
-					<input type="number" id="expected-return" />
+					<input
+						onChange={(e) => {
+							inputHandler("expected-return", e.target.value);
+						}}
+						type="number"
+						id="expected-return"
+					/>
 				</p>
 				<p>
 					<label htmlFor="duration">Investment Duration (years)</label>
-					<input type="number" id="duration" />
+					<input
+						onChange={(e) => {
+							inputHandler("duration", e.target.value);
+						}}
+						type="number"
+						id="duration"
+					/>
 				</p>
 			</div>
 			<p className="actions">
-				<button type="reset" className="buttonAlt">
+				<button onClick={resetHandler} type="reset" className="buttonAlt">
 					Reset
 				</button>
 				<button type="submit" className="button">
